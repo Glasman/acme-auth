656c6abfffef3681ee720c16b11dd153ab3f2cd0
markglasman <glasman.mark@gmail.com>
2024-06-04 14:46:59 -0400

    cleaned up server

--- db/client.js
+++ db/client.js
@@ -1,7 +1,6 @@
-// import { Client } from "pg";
 import pkg from 'pg';
 const { Client } = pkg;
 const client = new Client('postgres://localhost:5432/acme_auth')
 
-// module.exports = client;
+
 export default client;
